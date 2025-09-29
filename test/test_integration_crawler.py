fc9227ebc7a001946e0eed894cddcf36af4910cb
johannes <>
2025-09-29 12:58:17 +0200

    fixed test for new setup

--- test/test_integration_crawler.py
+++ test/test_integration_crawler.py
@@ -1,12 +1,18 @@
 
 import unittest
-from app.crawler import get_event_date_details_for_url, KANTINE_PROGRAM_URL
+from app.crawler import get_event_date_details_for_url, URL_MAPPING
 
 
 class TestIntegrationCrawler(unittest.TestCase):
 
-    def test_get_event_date_details_for_url(self):
-        items = get_event_date_details_for_url(KANTINE_PROGRAM_URL)
+    def test_get_event_date_details_for_kantine(self):
+        self.get_event_date_details_for_url("Kantine")
+
+    def test_get_event_date_details_for_klub(self):
+        self.get_event_date_details_for_url("Klub")
+
+    def get_event_date_details_for_url(self, key: str):
+        items = get_event_date_details_for_url(URL_MAPPING[key])
 
         assert len(
-            items) > 0, f"Expected at least one artist name for event URL: {KANTINE_PROGRAM_URL}"
+            items) > 0, f"Expected at least one artist name for event URL: {URL_MAPPING[key]}"
