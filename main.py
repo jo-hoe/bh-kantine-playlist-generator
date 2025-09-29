c4c2ab97d20d14883830a8bbf59cc117123c7f69
johannes <>
2025-09-29 12:35:59 +0200

    added SIGINT exit

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import os
-from datetime import datetime
+import logging
+import sys
+
 from app.crawler import get_event_date_details_for_url, KANTINE_PROGRAM_URL
 from app.playlist.spotify_playlist_generator import SpotifyPlaylistGenerator
 
 
 if __name__ == "__main__":
-    import logging
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.INFO)
+    try:
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.INFO)
 
-    playlist_name = os.environ.get(
-        "PLAYLIST_NAME", "Kantine Am Berghain: Next Up")
-    max_track_number_per_artist = int(
-        os.environ.get("MAX_TRACK_NUMBER_PER_ARTIST", 3))
-    is_running_in_container = os.environ.get(
-        "IS_RUNNING_IN_CONTAINER", "false").lower() == "true"
+        playlist_name = os.environ.get(
+            "PLAYLIST_NAME", "Kantine Am Berghain: Next Up")
+        max_track_number_per_artist = int(
+            os.environ.get("MAX_TRACK_NUMBER_PER_ARTIST", 3))
+        is_running_in_container = os.environ.get(
+            "IS_RUNNING_IN_CONTAINER", "false").lower() == "true"
 
-    tracks = get_event_date_details_for_url(KANTINE_PROGRAM_URL)
+        tracks = get_event_date_details_for_url(KANTINE_PROGRAM_URL)
 
-    generator = SpotifyPlaylistGenerator(
-        playlist_name,
-        max_track_number_per_artist,
-        os.path.join("cache", "token_cache.txt"),
-        is_running_in_container)
+        generator = SpotifyPlaylistGenerator(
+            playlist_name,
+            max_track_number_per_artist,
+            os.path.join("cache", "token_cache.txt"),
+            is_running_in_container)
 
-    generator.process(tracks)
+        generator.process(tracks)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        sys.exit(1)
+
+    logging.info("Script completed successfully.")
+    sys.exit(0)
